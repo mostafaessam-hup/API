512a117f2d009e74481eae4f19b6e4d0f13c0f9c
mostafaessam-hup <mostafaessam540@gmail.com>
2025-04-08 03:09:11 +0200

    login&register

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            $rules = [
+                'email' => 'required|email',
+                'password' => 'required',
+            ];
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return response()->json([
+                    'error' => true,
+                    'message' => 'Please provide valid email and password',
+                ], 401);
+            }
+            $credentials = $request->only('email', 'password');
+            $token = Auth::guard("api")->attempt($credentials);
+
+            if (!$token) {
+                return response()->json(['msg' => "error, email or password is wrong"]);
+            }
+            $user = Auth::guard("api")->user();
+            $user->token = $token;
+            return response()->json(['msg' => $user]);
+        } catch (\Exception $e) {
+            return  $e->getMessage();
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => true,
+                'message' => 'Please provide valid name, email and password',
+            ], 401);
+        }
+        $user = User::Create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password),
+        ]);
+        if ($user) {
+            return $this->login($request);
+        }
+        return response()->json(['msg' => "error"]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Auth\Events\Login;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::group(['namespace' => 'API'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
 });
